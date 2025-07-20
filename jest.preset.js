b1f52fce269c0e5c797b19803d51d28850213b62
Paul Spears <dpsthree@gmail.com>
2025-07-19 21:50:52 -0500

    continue with test prep

--- jest.preset.js
+++ jest.preset.js
@@ -1,3 +1,29 @@
 const nxPreset = require('@nx/jest/preset').default;
 
-module.exports = { ...nxPreset };
+module.exports = {
+  ...nxPreset,
+  collectCoverage: true,
+  coverageReporters: ['text', 'lcov', 'html'],
+  coverageDirectory: 'test-output/jest/coverage',
+  collectCoverageFrom: [
+    '**/*.{ts,tsx,js,jsx}',
+    '!**/*.spec.{ts,tsx,js,jsx}',
+    '!**/*.test.{ts,tsx,js,jsx}',
+    '!**/node_modules/**',
+    '!**/dist/**',
+    '!**/build/**',
+    '!**/coverage/**',
+    '!**/*.d.ts',
+    '!**/jest.config.{js,ts}',
+    '!**/test-setup.{js,ts}',
+    '!**/index.spec.{js,ts}'
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80
+    }
+  }
+};
