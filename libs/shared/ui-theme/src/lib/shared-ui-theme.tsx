d4a8607fe2c44fe33eb3ad19ed5cba84fb4c2cc8
Paul Spears <dpsthree@gmail.com>
2025-08-02 16:04:17 -0500

    remove unused boilerplate in theme library

--- libs/shared/ui-theme/src/lib/shared-ui-theme.tsx
+++
@@ -1,9 +0,0 @@
-export function SharedUiTheme() {
-  return (
-    <div className={styles['container']}>
-      <h1>Welcome to SharedUiTheme!</h1>
-    </div>
-  );
-}
-
-export default SharedUiTheme;
