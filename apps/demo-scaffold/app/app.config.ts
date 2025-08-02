97b452563adaf26ba4ef077c93cdaefb92d5c7b0
Paul Spears <dpsthree@gmail.com>
2025-08-02 12:53:28 -0500

    fix tailwind config

--- apps/demo-scaffold/app/app.config.ts
+++ apps/demo-scaffold/app/app.config.ts
@@ -25,6 +25,10 @@ export const getAppLinks: LinksFunction = () => [
     rel: 'stylesheet',
     href: 'https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap',
   },
+  {
+    rel: 'stylesheet',
+    href: '/styles.css',
+  }
 ];
 
 /**
