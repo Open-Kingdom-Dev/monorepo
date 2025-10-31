bcd6a6aca32c80949ca6c390743d3326c37d1bc2
Paul Spears <dpsthree@gmail.com>
2025-10-31 16:13:56 -0500

    convert frontend libraries to publishable libs
    use vite as the bundler

--- libs/shared/poly/util-types/jest.config.ts
+++ libs/shared/poly/util-types/jest.config.ts
@@ -1,19 +1,10 @@
-import { readFileSync } from 'fs';
-
-// Reading the SWC compilation config for the spec files
-const swcJestConfig = JSON.parse(
-  readFileSync(`${__dirname}/.spec.swcrc`, 'utf-8')
-);
-
-// Disable .swcrc look-up by SWC core because we're passing in swcJestConfig ourselves
-swcJestConfig.swcrc = false;
-
 export default {
-  displayName: '@open-kingdom/shared-poly-util-types',
+  displayName: '@open-kingdom/util-types',
   preset: '../../../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
-    '^.+\\.[tj]s$': ['@swc/jest', swcJestConfig],
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/react/babel'] }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
   coverageDirectory: 'test-output/jest/coverage',
