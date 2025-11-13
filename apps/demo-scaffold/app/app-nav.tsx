0d9eb762e3b5d11c37c68231ac90ec86351de885
dpsthree <dpsthree@gmail.com>
2025-11-12 19:15:02 -0600

    add app header

--- apps/demo-scaffold/app/app-nav.tsx
+++ apps/demo-scaffold/app/app-nav.tsx
@@ -2,13 +2,16 @@ import { NavLink } from 'react-router';
 
 export function AppNav() {
   return (
-    <nav>
-      <NavLink to="/" end>
-        Home
-      </NavLink>
-      <NavLink to="/about" end>
-        About
-      </NavLink>
-    </nav>
+    <>
+      <h1>App Header</h1>
+      <nav>
+        <NavLink to="/" end>
+          Home
+        </NavLink>
+        <NavLink to="/about" end>
+          About
+        </NavLink>
+      </nav>
+    </>
   );
 }
