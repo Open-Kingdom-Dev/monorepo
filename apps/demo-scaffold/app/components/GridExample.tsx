526d78776ca1d6d9d34411f9a41d0eadcf94be36
Samuel Ralak <thesamuelralak@gmail.com>
2025-11-13 20:35:28 +0300

    OKRE-43: Remove unnecessary useState hooks from GridExample

--- apps/demo-scaffold/app/components/GridExample.tsx
+++ apps/demo-scaffold/app/components/GridExample.tsx
@@ -1,24 +1,23 @@
-import { useState } from 'react';
 import { DataGrid } from '@open-kingdom/shared-frontend-ui-datagrid';
 
 export const GridExample = () => {
   // Row Data: The data to be displayed.
-  const [rowData] = useState([
+  const rowData = [
     { make: 'Tesla', model: 'Model Y', price: 64950, electric: true },
     { make: 'Ford', model: 'F-Series', price: 33850, electric: false },
     { make: 'Toyota', model: 'Corolla', price: 29600, electric: false },
     { make: 'Mercedes', model: 'EQA', price: 48890, electric: true },
     { make: 'Fiat', model: '500', price: 15774, electric: false },
     { make: 'Nissan', model: 'Juke', price: 20675, electric: false },
-  ]);
+  ];
 
   // Column Definitions: Defines & controls grid columns.
-  const [colDefs] = useState([
+  const colDefs = [
     { field: 'make' },
     { field: 'model' },
     { field: 'price' },
     { field: 'electric' },
-  ]);
+  ];
 
   const defaultColDef = {
     flex: 1,
