d255c880dc754bfd5f3f540f54735d2597c9db7f
dpsthree <dpsthree@gmail.com>
2025-11-12 22:49:43 -0600

    Revert "add app header"
    This reverts commit 0d9eb762e3b5d11c37c68231ac90ec86351de885.

--- apps/demo-scaffold/app/app-nav.tsx
+++ apps/demo-scaffold/app/app-nav.tsx
@@ -2,16 +2,13 @@ import { NavLink } from 'react-router';
 
 export function AppNav() {
   return (
-    <>
-      <h1>App Header</h1>
-      <nav>
-        <NavLink to="/" end>
-          Home
-        </NavLink>
-        <NavLink to="/about" end>
-          About
-        </NavLink>
-      </nav>
-    </>
+    <nav>
+      <NavLink to="/" end>
+        Home
+      </NavLink>
+      <NavLink to="/about" end>
+        About
+      </NavLink>
+    </nav>
   );
 }
